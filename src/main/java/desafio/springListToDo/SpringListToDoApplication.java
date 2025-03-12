6c3184e2930d4f05f309d0a7fb7f3894486c4339
ivens magno <ivensmagno@gmail.com>
2025-03-12 08:32:56 -0300

    adicação do swagger e uma documentaçao

--- src/main/java/desafio/springListToDo/SpringListToDoApplication.java
+++ src/main/java/desafio/springListToDo/SpringListToDoApplication.java
@@ -1,9 +1,15 @@
 package desafio.springListToDo;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(
+		title = "lista To Do",
+		version = "1.2",
+		description = "Back-end de uma aplicação da lista To Do com documentação e tratamentos de erro."))
 public class SpringListToDoApplication {
 
 	public static void main(String[] args) {
--- src/main/java/desafio/springListToDo/controller/ToDoController.java
+++ src/main/java/desafio/springListToDo/controller/ToDoController.java
@@ -2,12 +2,17 @@ package desafio.springListToDo.controller;
 
 import desafio.springListToDo.entity.ToDoEntity;
 import desafio.springListToDo.service.ToDoService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/toDos")
+@Tag(name = "Controller dos ToDos")
 public class ToDoController {
     private ToDoService toDoService;
 
@@ -15,23 +20,43 @@ public class ToDoController {
         this.toDoService = toDoService;
     }
 
+    @Operation(summary = "Cria um ToDo no DB", method = "POST")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Todo feito com sucesso"),
+            @ApiResponse(responseCode = "500", description = "Erro ao criar")
+    })
     @PostMapping
     List<ToDoEntity> create(@RequestBody ToDoEntity toDos) {
         toDoService.create(toDos);
         return toDoService.list();
     }
 
+    @Operation(summary = "Retorna uma lista de ToDos", method = "GET")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Retorno feito com sucesso"),
+    })
     @GetMapping
     List<ToDoEntity> list() {
         return toDoService.list();
     }
 
+    @Operation(summary = "Atualiza um ToDo no DB", method = "PUT")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Atualização feito com sucesso"),
+            @ApiResponse(responseCode = "500", description = "Erro ao atualizar"),
+            @ApiResponse(responseCode = "422", description = "Dados para requisição inválidos")
+    })
     @PutMapping
     List<ToDoEntity> update(@RequestBody ToDoEntity toDos) {
         toDoService.update(toDos);
         return toDoService.list();
     }
 
+    @Operation(summary = "Deleta um ToDo no DB", method = "DELETE")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Deletado feito com sucesso"),
+            @ApiResponse(responseCode = "500", description = "Erro ao deletar")
+    })
     @DeleteMapping("{id}")
     List<ToDoEntity> delete(@PathVariable("id") Long id) {
         toDoService.delete(id);
